1f9e3f19cfba741da19139d5f97192c097a5cb05
Kawaii-Killer <84580900+Kawaii-Killer@users.noreply.github.com>
2023-06-07 13:32:48 +0200

    Update filepreprocessing.py
    Added comments

--- filepreprocessing.py
+++ filepreprocessing.py
@@ -4,18 +4,22 @@ from nltk import word_tokenize
 # given pdffile path returns list of text sections with overlap, based on wordcount
 def pdf_get_text_chunks(file_path, chunk_size, overlap):
     chunks = []
+    # open pdf file based on path
     with open(file_path, 'rb') as file:
         reader = PyPDF2.PdfReader(file)
         num_pages = len(reader.pages)
         
+        # extract all text of pdf
         text = ""
         for page_num in range(num_pages):
             page = reader.pages[page_num]
             text += page.extract_text()
         
+        # tokenize text to words
         words = word_tokenize(text)
         num_words = len(words)
 
+        # make chuncks with overlap of words
         i = 0
         while i < num_words:
             chunk_end = min(i + chunk_size, num_words)
