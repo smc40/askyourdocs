ef4df0c62056257258ef5b8fdc1f9c2d3246206b
nicoapg <40054301+nicoapg@users.noreply.github.com>
2023-06-07 15:53:30 +0200

    pipelines - question and answer

--- pipelines.py
+++ pipelines.py
@@ -1,7 +1,7 @@
 from filepreprocessing import pdf_get_text_chunks
 import tqdm
 from embedding import get_embedding_sentence_transformer
-
+from similarity import model_qa, cosine_similarity
 
 def embedding_loaded_pdf(file_path, chunk_size, overlap):
 
@@ -15,3 +15,17 @@ def embedding_loaded_pdf(file_path, chunk_size, overlap):
         db_items.append([text, vector])
 
     return db_items
+
+
+def pipeline_return_question_and_answer(query, db_items)
+
+    # WE TRANSFORM THE QUERY TEXT INTO AN EMBEDDING
+    query_emb = get_embedding_sentence_transformer(query)
+
+    # GIVEN A DB_ITEMS COLLECTION, GET TOP MATCHES
+    top_items = cosine_similarity(query_emb, db_items, top_pick=5)
+
+    # PROVIDE QUESTION, TOP MATCHES ON THE EMBEDDED LIST OF ITEMS
+    answer = model_qa(query, top_items, model_card='google/flan-t5-small')
+
+    return answer
