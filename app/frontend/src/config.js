69abdd5373578deef31b6d26a670009f6d75ce54
alho94 <alexander.horst@swissmedic.ch>
2023-10-01 16:24:29 +0200

    improved docker compose file

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    backendUrl: 'http://localhost:8000',
+    backendUrl: process.env.BACKEND_URL || 'http://localhost:8000',
     easterEggTrigger: 'magic schnauz',
     easterEggTriggerMsg: 'magic schnauz 〰️',
 };
