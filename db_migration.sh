97b6d1a29d503d0094f130d4f116fc519e35b3ac
jaegglic <christoph.jaeggli@protonmail.com>
2023-10-11 14:39:58 +0100

    Removed extra db_migrate file

--- db_migration.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python -m askyourdocs storage migration -c "ayd_docs"
-python -m askyourdocs storage migration -c "ayd_texts"
-python -m askyourdocs storage migration -c "ayd_vecs"
-python -m askyourdocs storage migration -c "ayd_feedback"
-python -m askyourdocs pipeline ingest --source "docs" --commit
