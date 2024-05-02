c2a232bb714248f10a8c069b2bfb69e512038c98
bouldermaettel <bouldermaettel@gmail.com>
2024-05-02 14:00:48 +0200

    nginx files changed

--- app/backend/authentication.py
+++ app/backend/authentication.py
@@ -21,35 +21,28 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
         if request.url.path.startswith('/uploads') | request.url.path.startswith('/app') | request.url.path.startswith('/public') or request.url.path =="/" :
             response = await call_next(request)
             return response
+    
+        token = request.headers.get('Authorization')
+
+        if not token:
+            return JSONResponse({"error": "Unauthorized"}, 401)
+        
+        try:
+            authorization_info = keycloak_openid.introspect(token)
+
+        except Exception as e:
+            logging.error("Error raised from keycloak: ", e)
+            return JSONResponse({"error": "Unauthorized"}, 401)
+
+        if authorization_info.get("active") is not True:
+            return JSONResponse({"error": "Unauthorized"}, 401)
         
         request.state.userinfo = {
-            "id": "test-user-id",
-            "name": "Test User",
-            "username": "testuser",
-            "email": "testuser@example.com",
+            "id": authorization_info['sub'],
+            "name": authorization_info.get("name", None),
+            "username": authorization_info['preferred_username'],
+            "email": authorization_info.get("email"),
         }
-
-        # token = request.headers.get('Authorization')
-
-        # if not token:
-        #     return JSONResponse({"error": "Unauthorized"}, 401)
-        
-        # try:
-        #     authorization_info = keycloak_openid.introspect(token)
-
-        # except Exception as e:
-        #     logging.error("Error raised from keycloak: ", e)
-        #     return JSONResponse({"error": "Unauthorized"}, 401)
-
-        # if authorization_info.get("active") is not True:
-        #     return JSONResponse({"error": "Unauthorized"}, 401)
-        
-        # request.state.userinfo = {
-        #     "id": authorization_info['sub'],
-        #     "name": authorization_info.get("name", None),
-        #     "username": authorization_info['preferred_username'],
-        #     "email": authorization_info.get("email"),
-        # }
 
         response = await call_next(request)
         return response
