820a39738480e34d9275f753aeae96a707a32174
bouldermaettel <bouldermaettel@gmail.com>
2024-05-06 16:27:16 +0200

    udpated config.json

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    backendUrl: process.env.BACKEND_URL || 'http://localhost:8000',
+    backendUrl: process.env.BACKEND_URL || document.location.origin + '/api/', // 'http://localhost:8000',
     easterEggTrigger: 'magic schnauz',
     easterEggTriggerMsg: 'magic schnauz 〰️',
 };
