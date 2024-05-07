8ca561e823d7e428aed7ccaf9b8a90375fdc928d
bouldermaettel <bouldermaettel@gmail.com>
2024-05-07 08:30:15 +0200

    reverted site.yml file

--- app/frontend/src/config.js
+++ app/frontend/src/config.js
@@ -1,5 +1,5 @@
 export default {
-    backendUrl: process.env.BACKEND_URL ||  document.location.origin //'http://localhost:8000', //
+    backendUrl: process.env.BACKEND_URL || document.location.origin,
     easterEggTrigger: 'magic schnauz',
     easterEggTriggerMsg: 'magic schnauz 〰️',
 };
